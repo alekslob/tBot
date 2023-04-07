f0f0d4ef974979bce6e3be4c36a5d34853d77703
Слободчикова Александра <asalecslob@gmail.com>
2023-04-07 15:13:01 +0700

    другая таблица

--- db.py
+++ db.py
@@ -1,11 +1,20 @@
 import sqlite3
+import random as rnd
 
 class DBClient:
-    def __init__(self, dbname:str="data.db") -> None:
+    def __init__(self, count:int=1,dbname:str="data.db") -> None:
         self.connect = sqlite3.connect(dbname)
         self.cursor = self.connect.cursor()
-
-    def get_settings(self, settings):
+        self.clear_settings(count)
+        
+    def clear_settings(self, count:int=1):
+        settings=[['не по ссылке',0, 0]]
+        for i in range(1,count+1,1):
+            settings.append([f"по ссылке {i}",rnd.randint(1e5,1e6), 0])
+        return settings
+    
+    def get_settings(self, count:int=1):
+        settings = self.clear_settings(count)
         response = self.cursor.execute("select name from sqlite_master")
         if response.fetchone() is None: return settings
         # таблицы не существует
@@ -17,21 +26,26 @@ class DBClient:
 
         self.cursor.execute("select * from settings;")
         response = self.cursor.fetchall()
-        print(response)
+        # print(response)
+        settings = []
         for res in response:
-            settings[f"{res[0]}"] = res[1]
+            settings.append([res[1],res[2], res[3]])
         return settings
     
     def save_settings(self, settings) -> None:
-        prepare_command = "create table if not exists settings (id int primary key, name  count int);"
+        prepare_command = "create table if not exists settings (id int primary key, name varchar(225), option int, count int);"
         self.cursor.execute(prepare_command)
-        for _ in settings:
-            command = f"insert or replace into settings (id, count) values ({_}, {settings[_]});"
+        for i in range(len(settings)):
+            command = f"insert or replace into settings (id,name, option, count) values ({i},'{settings[i][0]}',{settings[i][1]}, {settings[i][2]});"
             self.cursor.execute(command)
         self.connect.commit()
 
-if __name__ == '__main__':
-    dbclient = DBClient()
-    settings = {'0': 0, '1': 0, '2':0}
-    dbclient.save_settings(settings)
-    print(dbclient.get_settings({}))
+# if __name__ == '__main__':
+#     dbclient = DBClient()
+#     set = dbclient.clear_settings(2)
+    # print(rnd.randint(1e5,1e6))
+    # if int(op)>0 and int(op)<len(set): print(True)
+    # else: print(False)
+    # settings = {'0': 0, '1': 0, '2':0}
+    # dbclient.save_settings(set)
+    # print(dbclient.get_settings())
--- tbot.py
+++ tbot.py
@@ -7,32 +7,39 @@ token = '5803984180:AAEN7i9qdI1VDceX29Kq8MEGzgt3pPrV4es'
 # options = ['1','2','3']
 # counts = [0,0,0]
 bot = telebot.TeleBot(token)
+count_linc = 2
 # logger = telebot.logger
 # telebot.logger.setLevel(logging.DEBUG)
 
 def addLinc(option):
     dbclient = DBClient()
-    options = dbclient.get_settings({})
-    if option in options:
-        options[option]+=1
-    else: options['0']+=1
-    dbclient.save_settings(options)
-    return options
+    options = dbclient.get_settings(count_linc)
+    try:
+        int_option = int(option)
+        if int_option>0 and int_option<=count_linc:
+            options[int_option][1]+=1
+        else: raise ValueError()
+    except ValueError:
+        options[0][1]+=1
+    finally:
+        dbclient.save_settings(options)
+        return options
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.from_user.id,'🤝')
-
+    print(message)
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text='Добавить к 1', url='https://t.me/phroil_bot?start=1')
-    btn2 = types.InlineKeyboardButton(text='Добавить к 2', url='https://t.me/phroil_bot?start=2')
+    btn1 = types.InlineKeyboardButton(text='Ссылка 1', url='https://t.me/phroil_bot?start=1')
+    btn2 = types.InlineKeyboardButton(text='Ссылка 2', url='https://t.me/phroil_bot?start=2')
     markup.add(btn1)
     markup.add(btn2)
     text = ""
     # print(message.text.split().pop())
     options = addLinc(message.text.split().pop())
+    print(options)
     for option in options:
-        text+=f"Перешли по ссылке {option}: {options[option]}\n"
+        text+=f"Перешли {option[0]}: {option[1]}\n"
     bot.send_message(message.from_user.id, text, reply_markup = markup)
 
 @bot.message_handler(content_types=['text'])
